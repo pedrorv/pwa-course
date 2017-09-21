9220279e02cd5256bc6ba4970b4cde53753d5ed8
Pedro Reis <pedroreisv@gmail.com>
2017-09-21 16:07:22 -0300

    clean old caches

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = 'static'
+var CACHE_DYNAMIC_NAME = 'dynamic'
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker...', event)
   event.waitUntil(
-    caches.open('static').then(function(cache) {
+    caches.open(CACHE_STATIC_NAME).then(function(cache) {
       console.log('[Service Worker] Precaching App Shell')
       cache.addAll([
         '/',
@@ -24,6 +27,18 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker...', event)
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key)
+            return caches.delete(key)
+          }
+        })
+      )
+    })
+  )
 
   return self.clients.claim()
 })
@@ -35,12 +50,14 @@ self.addEventListener('fetch', function(event) {
         return response
       }
 
-      return fetch(event.request).then(function(res) {
-        return caches.open('dynamic').then(function(cache) {
-          cache.put(event.request.url, res.clone())
-          return res
+      return fetch(event.request)
+        .then(function(res) {
+          return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+            cache.put(event.request.url, res.clone())
+            return res
+          })
         })
-      })
+        .catch(function(err) {})
     })
   )
 })
